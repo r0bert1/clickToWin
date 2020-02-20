5d4e190b2e8736b8d4c53d9865649a5379797525
r0bert1 <rpakkane@gmail.com>
2020-02-21 00:09:50 +0200

    proxy socket.io connection

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,10 @@ const config = {
     compress: true,
     port: 3000,
     proxy: {
-      '/api': 'http://localhost:3001',
+      '/socket.io': {
+        target: 'http://localhost:3001',
+        ws: true,
+      },
     },
   },
   devtool: 'source-map',
